d2ea8233d2b0f4e74bfacb026433d3382ee3879a
liqi-yu <liqi-yu@qq.com>
2021-01-25 20:05:24 +0800

    向右移动

---
+++ alien_invasion/alien_invasion/game_functions.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+def check_events(ship):
+	'''相应按键和鼠标事件'''
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			sys.exit()
+
+		elif event.type==pygame.KEYDOWN:
+			if event.key==pygame.K_RIGHT:
+				ship.moving_right=True
+
+		elif event.type==pygame.KEYUP:
+			if event.key==pygame.K_RIGHT:
+				ship.moving_right=False
+
+def update_screen(ai_settings,screen,ship):
+	'''更新屏幕上的图片，并切换到新屏幕'''
+	#每次循环都重绘屏幕
+	screen.fill(ai_settings.bg_color)
+	ship.blitme()
+
+	#让最近绘制的屏幕可见
+	pygame.display.flip()
